a0025e3c376357b589827422e7b6c88643478426
olawanlejoel <info4tobest2@gmail.com>
2024-08-03 11:28:44 +0100

    initial setup + navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,94 @@
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import logo from '@/public/images/logo.png';
+
+import { MdOutlineMenu, MdClose } from 'react-icons/md';
+
+const links = [
+	{
+		path: '/gig',
+		label: 'Post A Gig',
+	},
+	{
+		path: '/blog',
+		label: 'Blog',
+	},
+	{
+		path: '/about',
+		label: 'About Us',
+	},
+];
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const Icon = isOpen ? MdClose : MdOutlineMenu;
+
+	return (
+		<nav className="bg-white py-5">
+			<div className="container flex items-center justify-between ">
+				<div className="shrink-0">
+					<Link href="/">
+						<Image src={logo} alt="Artish logo" height={150} width={150} />
+					</Link>
+				</div>
+				<div className="hidden md:flex items-center gap-10">
+					<div className="flex gap-10">
+						{links.map(({ path, label }) => (
+							<Link key={path} href={path} className="font-semibold">
+								{label}
+							</Link>
+						))}
+					</div>
+					<div className="flex gap-5">
+						<Link
+							href="/sign-up"
+							className="font-semibold rounded-md border-2 border-black py-2 px-4"
+						>
+							Get Started
+						</Link>
+						<Link
+							href="/login"
+							className="font-semibold rounded-md border-2 border-black py-2 px-4 bg-black text-white"
+						>
+							Login
+						</Link>
+					</div>
+				</div>
+				<div className="md:hidden">
+					<Icon onClick={() => setIsOpen(!isOpen)} className="text-4xl" />
+				</div>
+			</div>
+			{isOpen && (
+				<div className="md:hidden py-10 px-5 absolute bg-white top-20 w-full">
+					<div className="flex flex-col gap-6">
+						{links.map(({ path, label }) => (
+							<Link key={path} href={path} className="font-semibold">
+								{label}
+							</Link>
+						))}
+						<div className="flex gap-5">
+							<Link
+								href="/sign-up"
+								className="font-semibold rounded-md border-2 border-black py-2 px-4"
+							>
+								Get Started
+							</Link>
+							<Link
+								href="/login"
+								className="font-semibold rounded-md border-2 border-black py-2 px-4 bg-black text-white"
+							>
+								Login
+							</Link>
+						</div>
+					</div>
+				</div>
+			)}
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+	return <div className="font-extrabold">page</div>;
+};
+
+export default page;
