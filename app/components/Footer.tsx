fba2dd28f030be975a17ae0755dc2278105bc102
emodot <emayeodavid@gmail.com>
2025-03-09 09:33:56 +0100

    fix: added links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,116 +9,131 @@ import { RiTwitterXFill } from 'react-icons/ri';
 
 const Footer = () => {
 	return (
-		<footer className="bg-[#FCFBFC] pt-10">
-			<div className="container flex justify-between flex-col md:flex-row">
-				<div className="mb-12 md:mb-0">
-					<div className="shrink-0">
-						<Link href="/">
-							<Image src={logo} alt="Artish logo" height={70} width={70} />
-						</Link>
-					</div>
-					<p className="mt-5">
-						<span className="font-semibold">123 Main St.</span>
-						<br />
-						Level 1, 123 Sample St., Lagos, Nigeria
-					</p>
-					<p className="mt-5">
-						<span className="font-semibold">Phone:</span> <br />
-						<Link href="tel:+234812344282" className="underline">
-							+234812344282
-						</Link>
-						<br />
-						<Link href="mailto:contact@artish.world" className="underline">
-							contact@artish.world
-						</Link>
-					</p>
-					<div className="flex space-x-4 mt-4">
-						<FaFacebook className="text-lg cursor-pointer" />
-						<FaInstagram className="text-lg cursor-pointer" />
-						<RiTwitterXFill className="text-lg cursor-pointer" />
-						<FaLinkedin className="text-lg cursor-pointer" />
-						<FaYoutube className="text-lg cursor-pointer" />
-					</div>
-				</div>
-				<div className="grid grid-cols-2 gap-10">
-					<div>
-						<ul className="space-y-2 font-semibold">
-							<li>
-								<Link href="/about-us" className="hover:underline">
-									About Us
-								</Link>
-							</li>
-							<li>
-								<Link href="/services" className="hover:underline">
-									Services
-								</Link>
-							</li>
-							<li>
-								<Link href="/pricing" className="hover:underline">
-									Pricing
-								</Link>
-							</li>
-							<li>
-								<Link href="/faq" className="hover:underline">
-									FAQ
-								</Link>
-							</li>
-							<li>
-								<Link href="/contact" className="hover:underline">
-									Contact Us
-								</Link>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<ul className="space-y-2 font-semibold">
-							<li>
-								<Link href="/blog" className="hover:underline">
-									Blog
-								</Link>
-							</li>
-							<li>
-								<Link href="/testimonials" className="hover:underline">
-									Testimonials
-								</Link>
-							</li>
-							<li>
-								<Link href="/terms" className="hover:underline">
-									Terms of Use
-								</Link>
-							</li>
-							<li>
-								<Link href="/privacy" className="hover:underline">
-									Privacy Policy
-								</Link>
-							</li>
-							<li>
-								<Link href="/cookies" className="hover:underline">
-									Cookie Policy
-								</Link>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div className="container mt-12 pb-10">
-				<div className="flex flex-wrap justify-center md:justify-between text-sm border-t pt-4 border-black">
-					<p>© 2023 ARTISH LLC. All rights reserved.</p>
-					<div className="flex gap-3 underline mt-2 md:mt-0">
-						<Link href="/privacy" className="hover:underline">
-							Privacy Policy
-						</Link>
-						<Link href="/terms" className="hover:underline">
-							Terms and Conditions
-						</Link>
-						<Link href="/cookies" className="hover:underline">
-							Cookie Settings
-						</Link>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+    <footer className="bg-[#FCFBFC] pt-10">
+      <div className="container flex justify-between flex-col md:flex-row">
+        <div className="mb-12 md:mb-0">
+          <div className="shrink-0">
+            <Link href="/">
+              <Image src={logo} alt="Artish logo" height={70} width={70} />
+            </Link>
+          </div>
+          <p className="mt-5">
+            <span className="font-semibold">123 Main St.</span>
+            <br />
+            Level 1, 123 Sample St., Lagos, Nigeria
+          </p>
+          <p className="mt-5">
+            <span className="font-semibold">Phone:</span> <br />
+            <Link href="tel:+234812344282" className="underline">
+              +234812344282
+            </Link>
+            <br />
+            <Link href="mailto:contact@artish.world" className="underline">
+              contact@artish.world
+            </Link>
+          </p>
+          <div className="flex space-x-4 mt-4">
+            <FaFacebook className="text-lg cursor-pointer" />
+            <FaInstagram className="text-lg cursor-pointer" />
+            <RiTwitterXFill className="text-lg cursor-pointer" />
+            <FaLinkedin className="text-lg cursor-pointer" />
+            <FaYoutube className="text-lg cursor-pointer" />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-10">
+          <div>
+            <ul className="space-y-2 font-semibold">
+              <li>
+                <Link
+                  href="https://www.app.artish.world/about-us"
+                  className="hover:underline"
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Services
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Pricing
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  FAQ
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="space-y-2 font-semibold">
+              <li>
+                <Link
+                  href="https://www.app.artish.world/blog"
+                  target="_blank"
+                  className="hover:underline"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Testimonials
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Terms of Use
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://www.app.artish.world/privacy-policy"
+                  target="_blank"
+                  className="hover:underline"
+                >
+                  Privacy Policy
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className="hover:underline">
+                  Cookie Policy
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container mt-12 pb-10">
+        <div className="flex flex-wrap justify-center md:justify-between text-sm border-t pt-4 border-black">
+          <p>© 2023 ARTISH LLC. All rights reserved.</p>
+          <div className="flex gap-3 underline mt-2 md:mt-0">
+            <Link
+              href="https://www.app.artish.world/privacy-policy"
+              target="_blank"
+              className="hover:underline"
+            >
+              Privacy Policy
+            </Link>
+            <Link href="#" className="hover:underline">
+              Terms and Conditions
+            </Link>
+            <Link href="#" className="hover:underline">
+              Cookie Settings
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/components/Home/Blog.tsx
+++ app/components/Home/Blog.tsx
@@ -31,49 +31,58 @@ const blogPosts = [
 
 const Blog = () => {
 	return (
-		<section className="bg-[#FCFBFC] py-28">
-			<div className="container">
-				<h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold">Blog</h2>
-				<p className="py-4 text-base ld:text-xl">
-					Discover the latest trends, tools, and resources in the creator
-					economy.
-				</p>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-12 ">
-					{blogPosts.map(
-						({ id, title, featureImg, readTime, excerpt, category }) => (
-							<div key={id} className="border-black border rounded-xl">
-								<div className="">
-									<Image
-										src={featureImg}
-										alt={title}
-										width={300}
-										height={300}
-										className="w-full object-cover h-[300px] rounded-tr-xl rounded-tl-xl"
-									/>
-								</div>
-								<div className="p-5 pb-10">
-									<div className="flex gap-3 items-center font-semibold">
-										<span className="bg-[#F4F4F4] p-2 rounded">{category}</span>
-										<span>{readTime} min read</span>
-									</div>
-									<h3 className="pt-4 pb-3 text-xl font-semibold">{title}</h3>
-									<p>{excerpt}</p>
-								</div>
-							</div>
-						)
-					)}
-				</div>
-				<div className="flex justify-end">
-					<Link
-						href="/login"
-						className="font-semibold rounded-md border border-black py-3 px-2 md:px-6 bg-white text-black "
-					>
-						View All
-					</Link>
-				</div>
-			</div>
-		</section>
-	);
+    <section className="bg-[#FCFBFC] py-28">
+      <div className="container">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold">Blog</h2>
+        <p className="py-4 text-base ld:text-xl">
+          Discover the latest trends, tools, and resources in the creator
+          economy.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-12 ">
+          {blogPosts.map(
+            ({ id, title, featureImg, readTime, excerpt, category }) => (
+              <Link
+                href="https://www.app.artish.world/blog"
+                target="_blank"
+                key={id}
+              >
+                <div className="border-black border rounded-xl">
+                  <div className="">
+                    <Image
+                      src={featureImg}
+                      alt={title}
+                      width={300}
+                      height={300}
+                      className="w-full object-cover h-[300px] rounded-tr-xl rounded-tl-xl"
+                    />
+                  </div>
+                  <div className="p-5 pb-10">
+                    <div className="flex gap-3 items-center font-semibold">
+                      <span className="bg-[#F4F4F4] p-2 rounded">
+                        {category}
+                      </span>
+                      <span>{readTime} min read</span>
+                    </div>
+                    <h3 className="pt-4 pb-3 text-xl font-semibold">{title}</h3>
+                    <p>{excerpt}</p>
+                  </div>
+                </div>
+              </Link>
+            )
+          )}
+        </div>
+        <div className="flex justify-end">
+          <Link
+            href="https://www.app.artish.world/blog"
+            target="_blank"
+            className="font-semibold rounded-md border border-black py-3 px-2 md:px-6 bg-white text-black "
+          >
+            View All
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
